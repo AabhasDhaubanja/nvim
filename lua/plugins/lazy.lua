54bbcd5e1b896261f716025f2afd4e7d620dba03
AabhasDhaubanja <aabhas41@gmail.com>
2023-11-04 10:36:32 +0545

    feat: add lsp-zero

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -12,15 +13,20 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
 	},
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
 		dependencies = { 'nvim-lua/plenary.nvim' }
