180ec6146bfc0b7230a123781b118aad44522964
AabhasDhaubanja <aabhas41@gmail.com>
2024-01-02 10:13:06 +0545

    feat: add nvim-tree toggle

--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -2,4 +2,5 @@ require("nvim-tree").setup {}
 
 vim.keymap.set('n', '<S-h>', vim.cmd.bp)
 vim.keymap.set('n', '<S-l>', vim.cmd.bn)
+vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<cr>')
 vim.keymap.set('n', '<leader>close', vim.cmd.bd)
