72b847a14860592f126e0664cc36c2d815bcff1a
AabhasDhaubanja <aabhas41@gmail.com>
2024-01-03 14:06:05 +0545

    feat: add up and down window switch remaps

--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -2,5 +2,7 @@ require("nvim-tree").setup {}
 
 vim.keymap.set('n', '<S-h>', vim.cmd.bp)
 vim.keymap.set('n', '<S-l>', vim.cmd.bn)
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<cr>')
 vim.keymap.set('n', '<leader>close', vim.cmd.bd)
